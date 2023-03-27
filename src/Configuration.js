21012249b04ecd5907fa29028907aafdaaf6dea3
Michael Elgavi <michael.elgavi@gmail.com>
2023-03-27 16:49:37 +0300

    Mobile view

--- src/Configuration.js
+++ src/Configuration.js
@@ -32,12 +32,12 @@ function Configuration({
 
 	return (
 		<Box sx={{ p: 1 }}>
-			<Box component="form" sx={{ display:'flex', flexDirection: 'column'}}>
+			<Box component="form" className="Configuration-form">
                 <TextField label="מיקום" variant="filled" fullWidth />
-                <Box sx={{ height: 196, width: 1 }}></Box>
+                <Box sx={{ minHeight: 196, minWidth: 256 }}></Box>
                 <Box sx={{ display:'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <TextField sx={{ mr: 0.5 }} label="קו רוחב" variant="filled" type="number" value={lat} onChange={(e) => setLat(e.target.value)} />
-                    <TextField sx={{ ml: 0.5 }} label="קו אורך" variant="filled" type="number" value={lon} onChange={(e) => setLon(e.target.value)} />
+                    <TextField sx={{ mr: 0.5, flex: 1 }} label="קו רוחב" variant="filled" type="number" value={lat} onChange={(e) => setLat(e.target.value)} />
+                    <TextField sx={{ ml: 0.5, flex: 1 }} label="קו אורך" variant="filled" type="number" value={lon} onChange={(e) => setLon(e.target.value)} />
                 </Box>
                 <TextField sx={{ mt: 1 }} label="גובה (מטרים)" variant="filled" type="number" value={elevation} onChange={(e) => setElevation(e.target.value)} />
                 
@@ -45,8 +45,8 @@ function Configuration({
                 
                 <TextField label="מספר עמודות" variant="filled" fullWidth type="number" value={columnCount} onChange={(e) => setColumnCount(e.target.value)} />
                 <Box sx={{ mt: 1, display:'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <TextField sx={{ mr: 0.5 }} label="גופן זמנים" variant="filled" type="number" value={timesFontSize} onChange={(e) => setTimesFontSize(e.target.value)} />
-                    <TextField sx={{ ml: 0.5 }} label="גופן הלכות" variant="filled" type="number" value={halachaFontSize} onChange={(e) => setHalachaFontSize(e.target.value)} />
+                    <TextField sx={{ mr: 0.5, flex: 1 }} label="גופן זמנים" variant="filled" type="number" value={timesFontSize} onChange={(e) => setTimesFontSize(e.target.value)} />
+                    <TextField sx={{ ml: 0.5, flex: 1 }} label="גופן הלכות" variant="filled" type="number" value={halachaFontSize} onChange={(e) => setHalachaFontSize(e.target.value)} />
                 </Box>
                 <TextField sx={{ mt: 1 }} label="כיתוב תחתון" variant="filled" fullWidth value={footerText} onChange={(e) => setfooterText(e.target.value)} />
 			    <Button sx={{ mt: 1 }} variant="contained" startIcon={<PrintIcon />} onClick={handlePrint} >הדפסה</Button>
